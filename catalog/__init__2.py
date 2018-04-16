3d549b153aad5aa60cf549121a83a580326c5439
Peter <petergns@outlook.com>
2018-04-16 09:08:28 -0400

    Add double quotes to if __name__ super secret key

--- catalog/__init__2.py
+++ catalog/__init__2.py
@@ -459,7 +459,7 @@ def showCatCharJSON(catalog_id, char_id):
     return jsonify(atChar=[catChar.serialize])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
-    app.secret_key = 'super_secret_key'
+    app.secret_key = "super_secret_key"
     app.run(host='0.0.0.0', port=5000)
